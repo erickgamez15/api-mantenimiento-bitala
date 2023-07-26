01e9ea3e27efba7c055b57b403943738128dea7f
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-26 15:14:35 -0600

    Configuración inicial del proyecto

---
+++ src/test/java/com/bitala/apiprueba/ApiPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitala.apiprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
