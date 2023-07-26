8fda177140a136648e1b8ab98dba5405a0c3e7af
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-26 15:36:16 -0600

    Configuración de la clase IEmpresaRepository

--- src/main/java/com/bitala/apiprueba/repository/IEmpresaRepository.java
+++ src/main/java/com/bitala/apiprueba/repository/IEmpresaRepository.java
@@ -1,4 +1,13 @@
 package com.bitala.apiprueba.repository;
 
-public interface IEmpresaRepository {
+import com.bitala.apiprueba.models.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IEmpresaRepository extends JpaRepository<Empresa, Long> {
+    List<Empresa> findByName(@Param("nombre") String nombre);
 }
