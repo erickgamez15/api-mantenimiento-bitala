91c887dfe53bab92890481725bb1a714ca23c340
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-26 16:26:14 -0600

    Metodos put, post y delete agregados a la clase EmpresaController. Correción en la clase IEmpresaRepository en la manera que se concatenan los datos

--- src/main/java/com/bitala/apiprueba/controllers/EmpresaController.java
+++ src/main/java/com/bitala/apiprueba/controllers/EmpresaController.java
@@ -3,8 +3,12 @@ package com.bitala.apiprueba.controllers;
 import com.bitala.apiprueba.models.Empresa;
 import com.bitala.apiprueba.repository.IEmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +29,19 @@ public class EmpresaController {
     public List<Empresa> findByName(@PathVariable("nombre") String nombre) {
         return empresaRepository.findByNombre(nombre);
     }
+
+    @PostMapping("/empresa")
+    public Empresa creatEmpresa(@RequestBody Empresa empresa){
+        return empresaRepository.save(empresa);
+    }
+    
+    @PutMapping("/empresa/{id}")
+    public Empresa updateEmpresa(@PathVariable Long id, @RequestBody Empresa empresa) {
+        return empresaRepository.save(empresa);
+    }
+
+    @DeleteMapping("/person/{id}")
+	public void deleteEmpresa(@PathVariable("id") Long id) {
+		empresaRepository.deleteById(id);
+	}
 }
