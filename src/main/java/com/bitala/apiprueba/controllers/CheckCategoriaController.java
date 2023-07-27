030c33e6febcaa0eaf21f605f9ecf44b6d09c0eb
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-27 13:36:25 -0600

    Creación y configuración de la clase CheckCategoriaController

---
+++ src/main/java/com/bitala/apiprueba/controllers/CheckCategoriaController.java
@@ -0,0 +1,52 @@
+package com.bitala.apiprueba.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.bitala.apiprueba.models.CheckCategoria;
+import com.bitala.apiprueba.repository.ICheckCategoriaRepository;
+
+@RestController
+@RequestMapping("/api/check-categoria")
+public class CheckCategoriaController {
+
+    //Anotación para inyección de dependencias
+    @Autowired
+    private ICheckCategoriaRepository checkCategoriaRepository;
+
+    //Objeto Empressa
+    CheckCategoria checkCategoria;
+
+    @GetMapping
+    public List<CheckCategoria> allCategorias(){
+        return checkCategoriaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CheckCategoria findById(@PathVariable("id") Long id){
+        return checkCategoriaRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public CheckCategoria createCategoria(@RequestBody CheckCategoria categoria){
+        return checkCategoriaRepository.save(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public CheckCategoria updateCategoria(@PathVariable Long id, @RequestBody CheckCategoria categoriaData) {
+        checkCategoria = checkCategoriaRepository.findById(id).orElse(null);
+        if(checkCategoria != null){
+            checkCategoria.setNombre(categoriaData.getNombre());
+            return checkCategoriaRepository.save(checkCategoria);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+	public void deleteCategoria(@PathVariable("id") Long id) {
+		checkCategoriaRepository.deleteById(id);
+	}
+}
