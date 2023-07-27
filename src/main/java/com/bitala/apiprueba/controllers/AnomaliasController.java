a9ef61e7096a8405a0ac03eb759419bccc08ba20
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-27 11:12:31 -0600

    Correción del tipo de dato List en findById en ChecksController

--- src/main/java/com/bitala/apiprueba/controllers/AnomaliasController.java
+++ src/main/java/com/bitala/apiprueba/controllers/AnomaliasController.java
@@ -17,10 +17,41 @@ public class AnomaliasController {
     private IAnomaliasRepository anomaliasRepository;
 
     //Objeto Anomalias
-    Anomalias anomalias;
+    Anomalias anomalia;
 
     @GetMapping
     public List<Anomalias> allAnomalias(){
         return anomaliasRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Anomalias findById(@PathVariable("id") Long id){
+        return anomaliasRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Anomalias createAnomalia(@RequestBody Anomalias anomalia){
+        return anomaliasRepository.save(anomalia);
+    }
+
+    @PutMapping("/{id}")
+    public Anomalias updateAnomalia(@PathVariable Long id, @RequestBody Anomalias anomaliaData){
+        anomalia = anomaliasRepository.findById(id).orElse(null);
+        if (anomalia != null) {
+            anomalia.setIdUnidad(anomaliaData.getIdUnidad());
+            anomalia.setIdChofer(anomaliaData.getIdChofer());
+            anomalia.setAnomalia(anomaliaData.getAnomalia());
+            anomalia.setDescripcion(anomaliaData.getDescripcion());
+            anomalia.setFecha(anomaliaData.getFecha());
+            anomalia.setEstatus(anomaliaData.getEstatus());
+            return anomaliasRepository.save(anomalia);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAnomalia(@PathVariable("id") Long id){
+        anomaliasRepository.deleteById(id);
     }
 }
