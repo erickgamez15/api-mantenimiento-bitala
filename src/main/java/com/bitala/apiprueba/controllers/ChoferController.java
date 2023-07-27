3be7db193d0021acf59007bc1b2f19591fba80d9
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-27 12:02:16 -0600

    Creación y configuración de la clase ChoferController

---
+++ src/main/java/com/bitala/apiprueba/controllers/ChoferController.java
@@ -0,0 +1,58 @@
+package com.bitala.apiprueba.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.bitala.apiprueba.models.Chofer;
+import com.bitala.apiprueba.repository.IChoferRepository;
+
+@RestController
+@RequestMapping("/api/chofer")
+public class ChoferController {
+
+    //Anotación para inyección de dependencias
+    @Autowired
+    private IChoferRepository choferRepository;
+    
+    //Objeto Chofer
+    Chofer chofer;
+
+    @GetMapping
+    public List<Chofer> allChofer(){
+        return choferRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Chofer findById(@PathVariable("id") Long id){
+        return choferRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Chofer createChofer(@RequestBody Chofer chofer){
+        return choferRepository.save(chofer);
+    }
+
+    @PutMapping("/{id}")
+    public Chofer updateChofer(@PathVariable Long id, @RequestBody Chofer choferData){
+        chofer = choferRepository.findById(id).orElse(null);
+        if (chofer != null) {
+            chofer.setNombre(choferData.getNombre());
+            chofer.setApellidos(choferData.getApellidos());
+            chofer.setCorreo(choferData.getCorreo());
+            chofer.setTelefono(choferData.getTelefono());
+            chofer.setPassword(choferData.getPassword());
+            chofer.setNoLista(choferData.getNoLista());
+            chofer.setFotoChofer(choferData.getFotoChofer());
+            return choferRepository.save(chofer);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteChofer(@PathVariable("id") Long id){
+        choferRepository.deleteById(id);
+    }
+}
