084a3a03aac8ca5e76a1e125ac31eddfd5790be3
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-27 13:18:31 -0600

    Creación y configuración de la clase CheckCategoria

---
+++ src/main/java/com/bitala/apiprueba/models/CheckCategoria.java
@@ -0,0 +1,46 @@
+package com.bitala.apiprueba.models;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "check_categoria")
+@EntityListeners(AuditingEntityListener.class)
+public class CheckCategoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+
+    private String nombre;
+
+    public CheckCategoria() {
+    }
+
+    public CheckCategoria(Long idCategoria, String nombre) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckCategoria [idCategoria=" + idCategoria + ", nombre=" + nombre + "]";
+    }
+}
