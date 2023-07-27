12e65c7a74935800ebe90afbcd1d77b327104957
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-27 13:21:09 -0600

    Creación y configuración de la interfaz  ICheckCategoriaRepository

---
+++ src/main/java/com/bitala/apiprueba/repository/ICheckCategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.bitala.apiprueba.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bitala.apiprueba.models.CheckCategoria;
+
+@Repository
+public interface ICheckCategoriaRepository extends JpaRepository<CheckCategoria, Long> {
+    
+}
